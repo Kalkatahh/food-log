bc578d3f3dc46e9c0f25c092cb1a193f2bd74dd0
Kalkatahh <simratkalkat@hotmail.com>
2021-02-11 00:56:26 -0600

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 
-secret_key = 'many random bytes'
+secret_key = <your secret key>
 env = 'dev'
-local_db = 'postgresql://simratkalkat:kalkat@localhost/Pantry'
-heroku_db = 'postgres://mxcahfurvpdhwo:28fc56a473f87e98643d5b7697562658c283fe0a8d37c6c5601a71a91ecb945d@ec2-54-172-17-119.compute-1.amazonaws.com:5432/d5lop7hnf6c78n'
+local_db = <your local/dev db>
+heroku_db = <the prod db>
