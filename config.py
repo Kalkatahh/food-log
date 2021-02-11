e8b59994f9344fb5f9bda1bb4a3a8ae09aa99975
Kalkatahh <simratkalkat@hotmail.com>
2021-02-11 00:55:21 -0600

    Add files via upload

---
+++ config.py
@@ -0,0 +1,5 @@
+
+secret_key = 'many random bytes'
+env = 'dev'
+local_db = 'postgresql://simratkalkat:kalkat@localhost/Pantry'
+heroku_db = 'postgres://mxcahfurvpdhwo:28fc56a473f87e98643d5b7697562658c283fe0a8d37c6c5601a71a91ecb945d@ec2-54-172-17-119.compute-1.amazonaws.com:5432/d5lop7hnf6c78n'
